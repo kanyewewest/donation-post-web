d676c12ab511b89b2eba6f81224f7f5d70f54154
dvckyy <sbatallanos.16@gmail.com>
2020-04-15 20:59:33 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { PostDescriptionComponent } from './post-description/post-description.component';
+import { PostViewerComponent } from './post-viewer/post-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostListComponent,
+    PostDetailComponent,
+    PostDescriptionComponent,
+    PostViewerComponent
   ],
   imports: [
     BrowserModule,
