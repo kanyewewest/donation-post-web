5887948b75c951060672bfbad85619e83ab11413
rubberdvcky <dvckyyyy@gmail.com>
2020-04-20 05:57:53 -0500

    Added models

---
+++ src/app/core/models/post.ts
@@ -0,0 +1,20 @@
+import { ITag } from './tag';
+import { IUser } from './user';
+
+export interface IPost {
+    id: number
+    title: string
+    description: string
+    address: string
+    latitude: number
+    longitude: number
+    thumbnailUrl: string
+    imgsUrl: string[]
+
+    creator: IUser
+    similar?: IPost[]
+    tags: ITag[]
+
+    dateExpiration: string
+    dateCreated: string
+}
